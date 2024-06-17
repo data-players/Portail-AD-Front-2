ffb574b9aeee44a95f327d3d6d23e07f3f11b4f0
simon louvet <simon.louvet.pro@gmail.com>
2024-06-17 14:00:23 +0200

    style

--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -12,11 +12,8 @@ const Map = () => {
   const [tooltipPosition, setTooltipPosition] = useState({ x: 0, y: 0 });
   const [hoveredDepartment, setHoveredDepartment] = useState(null);
   const mapRef = useRef();
-
-  // const [dimensions, setDimensions] = useState({
-  //   width: 0,
-  //   height: 0,
-  // });
+  const containerRef = useRef();
+  const [containerWidth, setContainerWidth] = useState(0);
 
   useEffect(() => {
     if (!geoData && !loadingGeoData) {
@@ -24,23 +21,40 @@ const Map = () => {
     }
   }, [geoData, loadingGeoData, dispatch]);
 
-  // useEffect(() => {
-  //   const handleResize = () => {
-  //     if (mapRef.current) {
-  //       console.log('mapRef.current:', mapRef.current);
-  //       console.log('offsetWidth:', mapRef.current.offsetWidth);
-  //       setDimensions({
-  //         width: mapRef.current.offsetWidth,
-  //         height: mapRef.current.offsetHeight,
-  //       });
-  //     }
-  //   };
+  useEffect(() => {
+    const handleResize = () => {
+      if (containerRef.current) {
+        const parentElement = containerRef.current;
+        const computedStyle = window.getComputedStyle(parentElement);
+        const parentHeight = parseFloat(computedStyle.height);
+        const parentWidth = parseFloat(computedStyle.width);
+        const aspectRatio = 205 / 210; // Largeur / Hauteur du ComposableMap
+        const calculatedWidth = parentHeight * aspectRatio;
 
-  //   window.addEventListener('resize', handleResize);
-  //   handleResize();
+        console.log('calculatedWidth', calculatedWidth);
+        console.log('parentWidth', parentWidth  );
 
-  //   return () => window.removeEventListener('resize', handleResize);
-  // }, []);
+        // Utiliser la plus petite valeur entre calculatedWidth et parentWidth
+        const newWidth = Math.min(calculatedWidth, parentWidth);
+
+        // Vérifier si la différence dépasse 10%
+        if (Math.abs(newWidth - containerWidth) / containerWidth > 0.1) {
+          setContainerWidth(newWidth);
+        }
+      }
+    };
+
+    const resizeObserver = new ResizeObserver(handleResize);
+    if (containerRef.current) {
+      resizeObserver.observe(containerRef.current);
+    }
+
+    return () => {
+      if (containerRef.current) {
+        resizeObserver.unobserve(containerRef.current);
+      }
+    };
+  }, [containerWidth]);
 
   const handleClick = (geography) => {
     navigate(`/search?refinementList[hasDepartment][0]=${encodeURIComponent(geography.properties.nom)}`);
@@ -60,74 +74,75 @@ const Map = () => {
   const handleMouseMove = (evt) => {
     setTooltipPosition({ x: evt.clientX, y: evt.clientY });
   };
-  // console.log('dimensions.width',dimensions.width);
+
   return (
-    <div ref={mapRef}>
-      <ComposableMap
-        width={210}
-        height={210}
-        projection="geoAzimuthalEqualArea"
-        projectionConfig={{
-          rotate: [-10.0, -52.0, 0],
-          center: [-5, -5.5],
-          scale:1200,
-          // scale: dimensions.width * 4,
-        }}
-      >
-        {geoData && (
-          <Geographies geography={geoData}>
-            {({ geographies }) =>
-              geographies.map((geo) => (
-                <Geography
-                  key={geo.rsmKey}
-                  geography={geo}
-                  onClick={() => handleClick(geo)}
-                  onMouseEnter={(evt) => handleMouseEnter(geo, evt)}
-                  onMouseLeave={handleMouseLeave}
-                  onMouseMove={handleMouseMove}
-                  style={{
-                    default: { fill: "#E1DFDD", outline: "none" },
-                    hover: { fill: "#B9FF66", outline: "none" },
-                    pressed: { fill: "#E42", outline: "none" },
-                  }}
-                />
-              ))
-            }
-          </Geographies>
-        )}
-        {geoData && (
-          <Geographies geography={geoData}>
-            {({ geographies }) =>
-              geographies.map((geo) => (
-                <path
-                  key={geo.rsmKey + "-border"}
-                  d={geo.svgPath}
-                  fill="none"
-                  stroke={"#000"}
-                  strokeWidth={hoveredDepartment === geo.rsmKey ? 0.5 : 0.2}
-                />
-              ))
-            }
-          </Geographies>
-        )}
-      </ComposableMap>
-      {tooltipContent && (
-        <div
-          style={{
-            position: 'fixed',
-            top: tooltipPosition.y - 50,
-            left: tooltipPosition.x - 50,
-            backgroundColor: 'white',
-            padding: '5px',
-            border: '1px solid black',
-            borderRadius: '3px',
-            pointerEvents: 'none',
-            whiteSpace: 'nowrap'
+    <div ref={containerRef} style={{flexGrow:1, flexShrink:1, flexBasis:'600px'}}>
+      <div ref={mapRef} style={{ width: `${containerWidth}px` }}>
+        <ComposableMap
+          width={205}
+          height={210}
+          projection="geoAzimuthalEqualArea"
+          projectionConfig={{
+            rotate: [-10.0, -52.0, 0],
+            center: [-5.15, -5.65],
+            scale: 1200,
           }}
         >
-          {tooltipContent}
-        </div>
-      )}
+          {geoData && (
+            <Geographies geography={geoData}>
+              {({ geographies }) =>
+                geographies.map((geo) => (
+                  <Geography
+                    key={geo.rsmKey}
+                    geography={geo}
+                    onClick={() => handleClick(geo)}
+                    onMouseEnter={(evt) => handleMouseEnter(geo, evt)}
+                    onMouseLeave={handleMouseLeave}
+                    onMouseMove={handleMouseMove}
+                    style={{
+                      default: { fill: "#E1DFDD", outline: "none" },
+                      hover: { fill: "#B9FF66", outline: "none" },
+                      pressed: { fill: "#E42", outline: "none" },
+                    }}
+                  />
+                ))
+              }
+            </Geographies>
+          )}
+          {geoData && (
+            <Geographies geography={geoData}>
+              {({ geographies }) =>
+                geographies.map((geo) => (
+                  <path
+                    key={geo.rsmKey + "-border"}
+                    d={geo.svgPath}
+                    fill="none"
+                    stroke={"#000"}
+                    strokeWidth={hoveredDepartment === geo.rsmKey ? 0.5 : 0.2}
+                  />
+                ))
+              }
+            </Geographies>
+          )}
+        </ComposableMap>
+        {tooltipContent && (
+          <div
+            style={{
+              position: 'fixed',
+              top: tooltipPosition.y - 50,
+              left: tooltipPosition.x - 50,
+              backgroundColor: 'white',
+              padding: '5px',
+              border: '1px solid black',
+              borderRadius: '3px',
+              pointerEvents: 'none',
+              whiteSpace: 'nowrap'
+            }}
+          >
+            {tooltipContent}
+          </div>
+        )}
+      </div>
     </div>
   );
 };
