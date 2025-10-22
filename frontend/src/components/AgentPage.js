5c1983db6f667630f86e4bc414577aa93118a887
simon louvet <simon.louvet.pro@gmail.com>
2025-10-22 12:26:36 +0200

    Add missing sentence about project deployment phase in disclaimer

--- frontend/src/components/AgentPage.js
+++ frontend/src/components/AgentPage.js
@@ -7,7 +7,7 @@ const AgentPage = () => {
     const disclaimerText = `
 Le Portail de l'Alimentation Durable est un projet numérique collectif, dont le portage, assuré dans sa première phase par Crisalim et Maurésiaterre, a été confié, en 2025, à Solagro. Le projet a déjà permis de rassembler près d'une dizaine d'acteurs qui contribuent à la construction du projet et centraliser les ressources de 6 structures.
 
-Dans le but de renforcer sa capacité à faciliter l'accès à une information fiable et pertinente en matière d'alimentation durable mais aussi le traitement et la restitution de l'information, le projet s'oriente vers le développement d'un agent IA en mesure de répondre à cet objectif. Un démonstrateur a été développé dans ce sens est vous est d'ores et déjà proposé à l'utilisation.
+Dans le but de renforcer sa capacité à faciliter l'accès à une information fiable et pertinente en matière d'alimentation durable mais aussi le traitement et la restitution de l'information, le projet s'oriente vers le développement d'un agent IA en mesure de répondre à cet objectif. Un démonstrateur a été développé dans ce sens est vous est d'ores et déjà proposé à l'utilisation. La phase de déploiement du projet qui débute vise à permettre l'amélioration de cet outil.
 
 Pour partager vos retours sur ce démonstrateur, vos attentes et/ou idées sur ce type d'outil, n'hésitez pas à écrire à : contact@portail-alimentation-durable.fr
 `;
